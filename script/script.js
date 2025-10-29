2226d894f10bc95bfb623eab4c50d071e27d22fe
simonw-dev <214550778+simon-wdev@users.noreply.github.com>
2025-10-29 11:58:31 +0100

    Setup files + calculator functions
    Wrote the 4 functions for add, substract, divide and multiply.

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let result;
+let num1;
+let num2;
+let operators = ["+", "-", "*", "/"]
+
+function add(num1, num2){
+    return result = num1 + num2;
+}
+
+function subtract(num1, num2){
+    return result = num1 - num2;
+}
+
+function multiply(num1, num2){
+    return result = num1 * num2;
+}
+
+function divide(num1, num2){
+    return result = num1 / num2;
+}
