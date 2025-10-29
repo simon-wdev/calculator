f9fa37448f22215d485346a1e22fcf21aa297c18
simonw-dev <214550778+simon-wdev@users.noreply.github.com>
2025-10-29 12:41:07 +0100

    Start to write operating function

--- script/script.js
+++ script/script.js
@@ -18,3 +18,17 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return result = num1 / num2;
 }
+
+//function get currentOp, index of operators
+
+function operate(num1, num2, currentOp){
+    if(currentOp == 0){
+        add(num1, num2)
+    }else if(currentOp == 1){
+        subtract(num1, num2)
+    }else if (currentOp == 2){
+        multiply(num1, num2)
+    }else{
+        divide(num1, num2)
+    }
+}
